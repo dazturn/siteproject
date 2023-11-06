b097071d9fad3823a3694bacbeebcaf525738db5
dazturn <daziahturner1@gmail.com>
2023-11-06 12:47:03 -0800

    Syntax fixes

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.IndexAPIView, name='home'),
-    path('aptitude/', views.ProfAPIView, name='aptitude'),
-    path('projects/', views.ProjectAPIView, name='projects'),
+    path('', views.IndexAPIView.as_view(), name='home'),
+    path('aptitude/', views.ProfAPIView.as_view(), name='aptitude'),
+    path('projects/', views.ProjectAPIView.as_view(), name='projects'),
 ]
