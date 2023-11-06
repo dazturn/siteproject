ebe0175d2e4070fad0cb1993695f606ccddbb2cf
dazturn <daziahturner1@gmail.com>
2023-11-06 11:50:49 -0800

    Serializers created in serializers.py

---
+++ myapp/serializers.py
@@ -0,0 +1,37 @@
+from rest_framework import serializers
+from .models import Profile, SocialMediaLink, Project, Skill, Education, Experience, Image
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = '__all__'
+
+class SMLSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SocialMediaLink
+        fields = '__all__'
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = '__all__'
+
+class SkillSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Skill
+        fields = '__all__'
+
+class EducationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Education
+        fields = '__all__'
+
+class ExperienceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Experience
+        fields = '__all__'
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        fields = '__all__'
