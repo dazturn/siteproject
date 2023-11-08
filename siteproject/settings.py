977bbd697ddf9ae962dcf44b41ba8a84254d3180
dazturn <daziahturner1@gmail.com>
2023-11-08 13:52:48 -0800

    Re-adding admin static files.

--- siteproject/settings.py
+++ siteproject/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 #Where collected files will be stored
-STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'admin_static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
