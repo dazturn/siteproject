cfb5f30dbed1378ddfb242bebbb653d9bdff69c8
dazturn <daziahturner1@gmail.com>
2023-11-10 12:26:41 -0800

    Added static file URL pattern

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
-# 
+
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('', views.IndexAPIView.as_view(), name='home'),
     path('aptitude/', views.ProfAPIView.as_view(), name='aptitude'),
     path('projects/', views.ProjectAPIView.as_view(), name='projects'),
 ]
+
+# Static files to serve
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
