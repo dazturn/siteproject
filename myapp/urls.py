aec31e42061a3d8c150556351808c71d565c1a7b
dazturn <daziahturner1@gmail.com>
2023-11-10 11:05:26 -0800

    Grouped views to URLs

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-
+# 
 urlpatterns = [
     path('', views.IndexAPIView.as_view(), name='home'),
     path('aptitude/', views.ProfAPIView.as_view(), name='aptitude'),
--- myapp/views.py
+++ myapp/views.py
@@ -5,51 +5,32 @@ from rest_framework.response import Response
 from .models import Profile, SocialMediaLink, Project, Skill, Education, Experience, Image
 from .serializers import ProfileSerializer, SMLSerializer, ProjectSerializer, SkillSerializer, EducationSerializer, ExperienceSerializer, ImageSerializer
 
-class ProfileListView(ListAPIView):
+# Profile, Social Media Link and Image views.
+class IndexAPIView(ListAPIView):
     serializer_class = ProfileSerializer
     permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
 
     def get_queryset(self):
-        return Profile.objects.all()
+        profiles = Profile.objects.all()
+        sml = SocialMediaLink.objects.all()
+        images = Image.objects.all()
+        return profiles.union(sml, images)
 
-class SocialMediaLinkListView(ListAPIView):
-    serializer_class = SMLSerializer
+# Skill, Education and Experience views.
+class ProfAPIView(ListAPIView):
+    serializer_class = SkillSerializer  # Use SkillSerializer or create a combined serializer for Skill, Education, and Experience
     permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
 
     def get_queryset(self):
-        return SocialMediaLink.objects.all()
+        skills = Skill.objects.all()
+        education = Education.objects.all()
+        experience = Experience.objects.all()
+        return skills.union(education, experience)
 
-class ProjectListView(ListAPIView):
+# The only view for this URL case so far.
+class ProjectAPIView(ListAPIView):
     serializer_class = ProjectSerializer
     permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
 
     def get_queryset(self):
         return Project.objects.all()
-
-class SkillListView(ListAPIView):
-    serializer_class = SkillSerializer
-    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
-
-    def get_queryset(self):
-        return Skill.objects.all()
-
-class EducationListView(ListAPIView):
-    serializer_class = EducationSerializer
-    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
-
-    def get_queryset(self):
-        return Education.objects.all()
-
-class ExperienceListView(ListAPIView):
-    serializer_class = ExperienceSerializer
-    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
-
-    def get_queryset(self):
-        return Experience.objects.all()
-
-class ImageViewList(ListAPIView):
-    serializer_class = ImageSerializer
-    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
-
-    def get_queryset(self):
-        return Image.objects.all()
