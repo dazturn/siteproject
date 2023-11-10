79c3b13cd34cfe44585b67794e69dca4f4baaac1
dazturn <daziahturner1@gmail.com>
2023-11-10 11:41:15 -0800

    Properly set static file directory.

--- siteproject/settings.py
+++ siteproject/settings.py
@@ -124,8 +124,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-# Serves collected static files
+# Serves collected static files from this folder name
 STATIC_URL = '/static/'
+
+# Static files to be collected
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'myapp', 'static')
+]
 
 #Where collected files will be stored
 STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
