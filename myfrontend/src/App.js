df4b0cb47b33f4c499f340798c38e651cb03a57b
dazturn <daziahturner1@gmail.com>
2023-11-14 12:37:51 -0800

    Directory change, final set of files.

---
+++ myfrontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import AppRouter from '.components/appRouter'
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <AppRouter />
+    </div>
+  );
+}
+
+export default App;
---
+++ myfrontend/src/components/appRouter.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Index from '.';
+import Aptitude from './aptitude';
+import Projects from './projects';
+
+const AppRouter = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route path="/" exact component={Index} />
+                <Route path="/aptitude" component={Aptitude} />
+                <Route path="projects" component={Projects}/>
+            </Switch>
+        </Router>
+    );
+};
