9da3c5c11dfc813342b9ae21b8bfd96ac664f527
dazturn <daziahturner1@gmail.com>
2023-11-15 10:45:17 -0800

    Fixed URL

--- myfrontend/src/aptitude.js
+++ myfrontend/src/aptitude.js
@@ -8,7 +8,7 @@ const Aptitude = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://localhost:8000');
+        const response = await axios.get('http://localhost:8000/aptitude');
         setData(response.data);
       } catch (error) {
         console.error('Error fetching data:', error);
