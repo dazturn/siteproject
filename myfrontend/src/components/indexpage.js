2a5eb1a4f03cbdd1e46d3e7d34b99e5e05a95576
dazturn <daziahturner1@gmail.com>
2023-11-18 12:16:24 -0800

    Rendered each data array for index.

--- myfrontend/src/components/indexpage.js
+++ myfrontend/src/components/indexpage.js
@@ -3,7 +3,7 @@ import './index.css';
 import axios from 'axios';
 
 const Indexpage = () => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState({ profiles: [], sml: [], images: [] });
 
   useEffect(() => {
     const fetchData = async () => {
@@ -21,18 +21,38 @@ const Indexpage = () => {
   return (
     <div>
       <h1>Welcome to my Portfolio Homepage!</h1>
-      {data.length > 0 ? (
-        data.map((item) => (
-          <div key={item.id}>
-            <h2>{item.owner_name}</h2>
-            <img src={item.image} alt="" />
-            <p>{item.owner_bio}</p>
-            <p>Contact me Today: {`${item.contact_info} ${item.platform}`}</p>
+      
+      {/* Render profile(s) */}
+      <div>
+        {data.profiles.map((profile) => (
+          <div key={profile.id}>
+            <h2>{profile.owner_name}</h2>
+            <p>{profile.owner_bio}</p>
+            <p>Contact me Today: {profile.contact_info}</p>
           </div>
-        ))
-      ) : (
-        <p>Loading...</p>
-      )}
+        ))}
+      </div>
+
+      {/* Render social media link(s) */}
+      <div>
+        {data.sml.map((socialMedia) => (
+          <div key={socialMedia.id}>
+            <p>Platform: {socialMedia.platform}</p>
+            <p>URL: {socialMedia.url}</p>
+            <img src={socialMedia.icon} alt="" />
+          </div>
+        ))}
+      </div>
+
+      {/* Render image(s) */}
+      <div>
+        {data.images.map((image) => (
+          <div key={image.id}>
+            <p>Image Title: {image.image_title}</p>
+            <img src={image.image} alt="" />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };  
